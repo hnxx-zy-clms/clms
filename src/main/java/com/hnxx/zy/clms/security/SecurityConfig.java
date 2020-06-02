183b85b6dbb8cadbc728e6597735dc89eaf6b0b0
DeepBluet <1360034559@qq.com>
2020-06-03 05:11:11 +0800

    DeepBluet 配置修改

--- src/main/java/com/hnxx/zy/clms/security/SecurityConfig.java
+++ src/main/java/com/hnxx/zy/clms/security/SecurityConfig.java
@@ -109,10 +109,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 如果有允许匿名的url，填在下面
                 .antMatchers("/code/sms","/login","/register","/druid/**","/home","/callback","/github/**").permitAll()
                 .anyRequest()
-                .permitAll()
+//                .permitAll()
                 //允许所有请求通过(开发测试时设置，不设置登录,else测试要抓狂)
                 //坑爹de认证方法
-//                 .authenticated()
+                 .authenticated()
                 .and()
                 //设置登录方式,和登录接口，登录请求方式必须是Post
                 .formLogin().loginPage("/login")
