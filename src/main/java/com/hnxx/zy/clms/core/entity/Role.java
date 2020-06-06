2ddd428fe5e9a53e97ef714a1438fb27d49aae05
DeepBluet <1360034559@qq.com>
2020-06-06 16:17:48 +0800

    DeepBluet 完善了用户和角色模块

--- src/main/java/com/hnxx/zy/clms/core/entity/Role.java
+++ src/main/java/com/hnxx/zy/clms/core/entity/Role.java
@@ -32,6 +32,11 @@ public class Role {
     private int rolePositionId;
 
     /**
+     * 角色描述
+     */
+    private String roleDescription;
+
+    /**
      * 创建时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
